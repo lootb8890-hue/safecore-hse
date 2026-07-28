3ead3bb7f5706e14e94dcc18d26a345935d37332
lootb8890-hue <loo.puge@gmail.com>
2026-07-28 17:18:42 +0300

    chore: clean unused imports and code refinement

--- flutter_app/lib/features/emergency/emergency_red_button.screen.dart
+++ flutter_app/lib/features/emergency/emergency_red_button.screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import '../../core/network/socket_service.dart';
-import '../../core/theme/branding_engine.dart';
 
 class EmergencyRedButtonScreen extends StatefulWidget {
   const EmergencyRedButtonScreen({Key? key}) : super(key: key);
